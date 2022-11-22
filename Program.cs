f4c635a8cddfcfaf80a0570f9605aca2d16d5e10
MDTVisit <112145096+MDTVisit@users.noreply.github.com>
2022-11-22 22:03:15 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+static void Main (string[] args)
+{
+       Queue<char> fruit = new Queue<char>();
+       
+       while (true) {
+       char f = char.Parse(Console.ReadLine());
+     if (f == 'L') {fruit.Push('1');}
+     if (f == 'M') {fruit.Push('2');}
+     if (f == 'S') {fruit.Push('3');}
+
+      Console.Write(fruit.GetLength());
+break;
+       }
+
+while(fruit.GetLength()!=0)
+{
+char f = fruit.Pop();
+Console.Write(f);
+if (f == '1' ) {fruit.Push('2');fruit.Push('2');}
+else if (f == '2' ) {fruit.Push('3');fruit.Push('3');fruit.Push('3');}
+}             
+}
+}
